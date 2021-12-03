c362d1cccc628a5bd4c06b68d11a9ffa85bba41e
gustavo.caixeta <gustavo.caixeta42@gmail.com>
2021-12-02 23:11:44 -0300

    projeto bytebank envio de dados via httpclient com json server

---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Transferencia } from '../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
+
+@Component({
+  selector: 'app-extrato',
+  templateUrl: './extrato.component.html',
+  styleUrls: ['./extrato.component.scss']
+})
+export class ExtratoComponent implements OnInit {
+
+ transferencia: any[];
+
+
+  constructor(private service: TransferenciaService) { }
+
+  ngOnInit(): void {
+   this.service.todas().subscribe((transferencias: Transferencia[])=>{
+      console.table(transferencias)
+      this.transferencia = transferencias
+   })
+  }
+
+}
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,12 +10,33 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>()
+
   valor: number;
   destino: number;
 
+  constructor(
+    private service: TransferenciaService,
+    private router: Router
+  ) {
+
+  }
+
   transferir() {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    let valorEmitido: Transferencia = { valor: this.valor, destino: this.destino }
+
+    this.service.adicionar(valorEmitido).subscribe(result => {
+
+      console.log(result)
+      this.router.navigateByUrl('extrato')
+    }, error => {
+      console.log(error)
+    })
+  }
+
+  limparCampos() {
+    this.valor = 0
+    this.destino = 0
   }
 }
